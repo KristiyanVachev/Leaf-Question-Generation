dcf99a6d960b7f99772b40d5ef6c3fb50b4a0367
typhon04@gmail.com <!lekiti00drew>
2021-10-05 22:36:03 +0300

    Integrated a multitask model into the app

---
+++ app/question_generation.py
@@ -0,0 +1,108 @@
+from logging import NullHandler
+from typing import List, Dict, Tuple
+import tqdm.notebook as tq
+from tqdm.notebook import tqdm
+import json
+import pandas as pd
+import numpy as np
+
+import torch
+from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from transformers import (
+    AdamW,
+    T5ForConditionalGeneration,
+    T5TokenizerFast as T5Tokenizer
+    )
+
+# Constants
+MODEL_NAME = 't5-small'
+LEARNING_RATE = 0.0001
+SOURCE_MAX_TOKEN_LEN = 300
+TARGET_MAX_TOKEN_LEN = 80
+SEP_TOKEN = '<sep>'
+TOKENIZER_LEN = 32101 #after adding the new <sep> token
+
+# QG Model
+class QGModel(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME, return_dict=True)
+        self.model.resize_token_embeddings(TOKENIZER_LEN) #resizing after adding new tokens to the tokenizer
+
+    def forward(self, input_ids, attention_mask, labels=None):
+        output = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+        return output.loss, output.logits
+
+    def training_step(self, batch, batch_idx):
+        input_ids = batch['input_ids']
+        attention_mask = batch['attention_mask']
+        labels = batch['labels']
+        loss, output = self(input_ids, attention_mask, labels)
+        self.log('train_loss', loss, prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        input_ids = batch['input_ids']
+        attention_mask = batch['attention_mask']
+        labels = batch['labels']
+        loss, output = self(input_ids, attention_mask, labels)
+        self.log('val_loss', loss, prog_bar=True, logger=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        input_ids = batch['input_ids']
+        attention_mask = batch['attention_mask']
+        labels = batch['labels']
+        loss, output = self(input_ids, attention_mask, labels)
+        self.log('test_loss', loss, prog_bar=True, logger=True)
+        return loss
+  
+    def configure_optimizers(self):
+        return AdamW(self.parameters(), lr=LEARNING_RATE)
+
+
+class QuestionGenerator():
+    def __init__(self):
+        self.tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME).add_tokens(SEP_TOKEN)
+        # print('tokenizer len before: ', len(self.tokenizer))
+        self.tokenizer.add_tokens(SEP_TOKEN)
+        # print('tokenizer len after: ', len(self.tokenizer))
+        self.tokenizer_len = len(self.tokenizer)
+
+        checkpoint_path = 'app/data/8.4-multitask-qg-ag.ckpt'
+        self.qg_model = QGModel.load_from_checkpoint(checkpoint_path)
+        self.qg_model.freeze()
+        self.qg_model.eval()
+
+    def generate(self, answer: str, context: str) -> str:
+        source_encoding = self.tokenizer(
+            '{} {} {}'.format(answer, SEP_TOKEN, context),
+            max_length=SOURCE_MAX_TOKEN_LEN,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            add_special_tokens=True,
+            return_tensors='pt'
+        )
+
+        generated_ids = self.qg_model.model.generate(
+            input_ids=source_encoding['input_ids'],
+            attention_mask=source_encoding['attention_mask'],
+            num_beams=16,
+            max_length=TARGET_MAX_TOKEN_LEN,
+            repetition_penalty=2.5,
+            length_penalty=1.0,
+            early_stopping=True,
+            use_cache=True
+        )
+
+        preds = {
+            self.tokenizer.decode(generated_id, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+            for generated_id in generated_ids
+        }
+
+        return ''.join(preds)
---
+++ main.py
@@ -0,0 +1,29 @@
+import textwrap
+from app.question_generation import *
+
+def show_result(generated: str, answer: str, context:str, original_question: str = ''):
+    
+    print('Context:')
+
+    for wrap in textwrap.wrap(context, width=120):
+        print(wrap)
+    print()
+
+    print('Question:')
+    print(generated)
+
+    print('Answer:')
+    print(answer)
+    print('-----------------------------')
+
+
+context = '''The koala or, inaccurately, koala bear[a] (Phascolarctos cinereus), is an arboreal herbivorous marsupial native to Australia. It is the only extant representative of the family Phascolarctidae and its closest living relatives are the wombats, which are members of the family Vombatidae. The koala is found in coastal areas of the mainland's eastern and southern regions, inhabiting Queensland, New South Wales, Victoria, and South Australia. It is easily recognisable by its stout, tailless body and large head with round, fluffy ears and large, spoon-shaped nose. The koala has a body length of 60–85 cm (24–33 in) and weighs 4–15 kg (9–33 lb). Fur colour ranges from silver grey to chocolate brown. Koalas from the northern populations are typically smaller and lighter in colour than their counterparts further south. These populations possibly are separate subspecies, but this is disputed.'''
+real_answer = 'Koala'
+input_answer = '[MASK]'
+
+#TODO 
+question_generator = QuestionGenerator()
+
+generated = question_generator.generate(input_answer, context)
+
+show_result(generated, real_answer, context)
